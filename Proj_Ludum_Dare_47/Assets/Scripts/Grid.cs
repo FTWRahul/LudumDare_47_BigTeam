e5716fd2c2ff626fec03854cf771b3528b4f21f2
GitWillG <wgrant14@hastings.edu>
2020-10-02 23:21:29 -0400

    Basic Grid functionality
    Created a generate grid function
    Created a script to hover and select grid spaces

---
+++ Proj_Ludum_Dare_47/Assets/Scripts/Grid.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public int width;
+    public int depth;
+    public float sideLength = 1f;
+    public GameObject gridPref;
+
+    [SerializeField]
+    public GameObject[,] gridArray;
+
+
+    public void Start()
+    {
+        genGrid();
+    }
+
+
+    [ContextMenu("Generate Grid")]
+    public void genGrid(/*int width, int depth*/)
+    {
+        
+        gridArray = new GameObject[width, depth];
+        for (int x = 0; x< gridArray.GetLength(0); x++)
+        {
+            for (int z = 0; z < gridArray.GetLength(1); z++)
+            {
+                float xPos = x * (sideLength + 0.01f);
+                float zPos = z * (sideLength + 0.01f);
+                GameObject gridSquare = (GameObject)Instantiate(gridPref, new Vector3(xPos, 0f, zPos), Quaternion.identity);
+                gridSquare.name = "Square " + x + ":" + z;
+                gridSquare.transform.SetParent(this.gameObject.transform); 
+                gridArray[x, z] = gridSquare.gameObject;
+            }
+
+        }
+
+    }
+
+}
---
+++ Proj_Ludum_Dare_47/Assets/Scripts/mouseFunctionality.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseFunctionality : MonoBehaviour
+{
+    public Material hoverMat;
+    public Material selectedMat;
+    public Material oldMat;
+    public Material invalidMat;
+    public Transform selectedGrid;
+
+    public int currentMask;
+
+    public Transform hoveredGrid;
+    Renderer selectionRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentMask = 1 << 8;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hoveredGrid != null)
+        {
+            //get the rennderer of the last hovered targed
+            selectionRenderer = hoveredGrid.GetComponent<Renderer>();
+            if (hoveredGrid.tag != "filledGrid")
+            {
+                //reset it
+                selectionRenderer.material = oldMat;
+            }
+            else if (hoveredGrid.tag == "filledGrid")
+            { 
+                //reset it
+                selectionRenderer.material = selectedMat;
+            }
+
+
+            //null the old target
+            hoveredGrid = null;
+        }
+       
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, currentMask))
+        {
+            //save the selection and get its renderer
+            var selection = hit.transform;
+            selectionRenderer = selection.GetComponent<Renderer>();
+            hoveredGrid = selection;
+            oldMat = selectionRenderer.material;
+
+            // Hovering
+            if (selectionRenderer != null && hoveredGrid.tag != "filledGrid")
+            {
+                selectionRenderer.material = hoverMat;
+
+            }
+            else if (selectionRenderer != null && hoveredGrid.tag == "filledGrid")
+            {
+                selectionRenderer.material = invalidMat;
+
+            }
+            if (Input.GetMouseButtonUp(0) && hoveredGrid != null && hoveredGrid.tag != "filledGrid")
+            {
+                selectGrid(hoveredGrid.gameObject);
+            }
+
+        }
+    }
+
+    private void selectGrid(GameObject selObject)
+    {
+        selObject.GetComponent<Renderer>().material = selectedMat;
+        selObject.tag = "filledGrid";
+    }
+}
